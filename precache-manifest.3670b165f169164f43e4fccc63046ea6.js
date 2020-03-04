a04555d5827e9f2db01c2e901903daf46882d189
Actireon <eheieiei37@gmail.com>
2020-03-04 16:43:03 +0200

    Updates

---
+++ precache-manifest.3670b165f169164f43e4fccc63046ea6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3420b8db70a7d23a10c398f51c337342",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb6706a455493e354ea1",
+    "url": "./static/js/2.a2437a3a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.a2437a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f27305d4fe52858119",
+    "url": "./static/js/main.483a9286.chunk.js"
+  },
+  {
+    "revision": "761b68421a787eb401f4",
+    "url": "./static/js/runtime-main.cfdadacd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69c980133d8746ec7592bc9413534b1f.js"
+  "./precache-manifest.3670b165f169164f43e4fccc63046ea6.js"
 );
 
 self.addEventListener('message', (event) => {
